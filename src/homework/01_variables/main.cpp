e6c9c0731a980cf9b6c360568b72a1a9de834c9b
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-06-13 20:04:06 -0500

    Prompt user with cout

--- src/homework/01_variables/main.cpp
+++ src/homework/01_variables/main.cpp
@@ -15,8 +15,10 @@ Call multiply_numbers with 10 and 10 parameter values and display function resul
 int main()
 {
 	double meal_amount, tip_rate, tip_amount, tax_amount, total;
+	cout<<"Enter meal amount: ";
 	cin>> meal_amount;
 	tax_amount = get_sales_tax_amount(meal_amount);
+	cout<<"Enter tip rate (decimal): ";
 	cin>> tip_rate;
 	tip_amount = get_tip_amount(meal_amount, tip_rate);
 	total = tip_amount + tax_amount + meal_amount;
