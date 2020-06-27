1add6f1446047227a258d47a0da625d37e3193c6
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-06-26 20:33:00 -0500

    hw4 grading: test cases incorrect

--- test/homework_test/tic_tac_toe_test/tic_tac_toe_test.cpp
+++ test/homework_test/tic_tac_toe_test/tic_tac_toe_test.cpp
@@ -11,23 +11,23 @@ TEST_CASE("Verify game ends when the board is full")
 	TicTacToe tic_tac_toe;
 	tic_tac_toe.start_game("X");
 
-	tic_tac_toe.mark board(1);
-	REQUIRE(tic_tac_toe.game_over() == false};
-	tic_tac_toe.mark board(2);
-	REQUIRE(tic_tac_toe.game_over() == false};
-	tic_tac_toe.mark board(3);
-	REQUIRE(tic_tac_toe.game_over() == false};
-	tic_tac_toe.mark board(4);
-	REQUIRE(tic_tac_toe.game_over() == false};
-	tic_tac_toe.mark board(5);
-	REQUIRE(tic_tac_toe.game_over() == false};
-	tic_tac_toe.mark board(6);
-	REQUIRE(tic_tac_toe.game_over() == false};
-	tic_tac_toe.mark board(7);
-	REQUIRE(tic_tac_toe.game_over() == false};
-	tic_tac_toe.mark board(9);
-	REQUIRE(tic_tac_toe.game_over() == false};
-	tic_tac_toe.mark board(8);
-	REQUIRE(tic_tac_toe.game_over() == true};
+	tic_tac_toe.mark_board(1);
+	REQUIRE(tic_tac_toe.game_over() == false);
+	tic_tac_toe.mark_board(2);
+	REQUIRE(tic_tac_toe.game_over() == false);
+	tic_tac_toe.mark_board(3);
+	REQUIRE(tic_tac_toe.game_over() == false);
+	tic_tac_toe.mark_board(4);
+	REQUIRE(tic_tac_toe.game_over() == false);
+	tic_tac_toe.mark_board(5);
+	REQUIRE(tic_tac_toe.game_over() == false);
+	tic_tac_toe.mark_board(6);
+	REQUIRE(tic_tac_toe.game_over() == false);
+	tic_tac_toe.mark_board(7);
+	REQUIRE(tic_tac_toe.game_over() == false);
+	tic_tac_toe.mark_board(9);
+	REQUIRE(tic_tac_toe.game_over() == false);
+	tic_tac_toe.mark_board(8);
+	REQUIRE(tic_tac_toe.game_over() == true);
 	
 }
